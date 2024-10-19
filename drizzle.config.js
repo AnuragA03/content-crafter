80b7f1b96c51e09cc2043ff6712f2e4dff4fe492
AnuragA03 <anuragadhikari2003@gmail.com>
2024-10-19 17:58:13 +0530

    database schema added and connected to neon postgresql

---
+++ drizzle.config.js
@@ -0,0 +1,10 @@
+export default {
+    dialect: 'postgresql',
+    schema: './utils/db/schema.ts',
+    out: './drizzle',
+
+    dbCredentials: {
+        url: "postgresql://contentcraftai_owner:jPbWxE0Zst8Y@ep-lively-silence-a5iq1nse.us-east-2.aws.neon.tech/contentcraftai?sslmode=require",
+        connectionString: "postgresql://contentcraftai_owner:jPbWxE0Zst8Y@ep-lively-silence-a5iq1nse.us-east-2.aws.neon.tech/contentcraftai?sslmode=require",
+    },
+}
